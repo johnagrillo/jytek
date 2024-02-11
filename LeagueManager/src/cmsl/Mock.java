0a45a0d1a8988e8de48aebc6d8170e7d092db24f
Grillo, John <jgrillo6@gatech.edu>
2024-02-10 19:12:17 -0500

    move tables to hytek.tm

--- LeagueManager/src/cmsl/Mock.java
+++ LeagueManager/src/cmsl/Mock.java
@@ -1,7 +1,6 @@
 package cmsl;
 
-import com.healthmarketscience.jackcess.Row;
-import hytek.*;
+import com.healthmarketscience.jackcess.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,52 +21,50 @@ public final class Mock {
             // hash map of teams
             //
 
-            final var teams = new HashMap<Integer, hytek.Team>();
-            for (final Row row : db.getTable(hytek.Team.NAME)) {
-                hytek.Team team = new hytek.Team(row);
+            final var teams = new HashMap<Integer, hytek.tm.Team>();
+            for (final Row row : db.getTable(hytek.tm.Team.NAME)) {
+                hytek.tm.Team team = new hytek.tm.Team(row);
                 teams.put(team.getTeam(), team);
             }
 
-            final var mtevente = new HashMap<Integer, hytek.Mtevente>();
-            for (final Row row : db.getTable(Mtevente.NAME)) {
-                Mtevente eve = new hytek.Mtevente(row);
+            final var mtevente = new HashMap<Integer, hytek.tm.Mtevente>();
+            for (final Row row : db.getTable(hytek.tm.Mtevente.NAME)) {
+                hytek.tm.Mtevente eve = new hytek.tm.Mtevente(row);
                 mtevente.put(eve.getMtevent(), eve);
-                System.out.println(eve);
             }
 
-            final var mtevent = new HashMap<Integer, hytek.Mtevent>();
-            for (final Row row : db.getTable(Mtevent.NAME)) {
-                Mtevent ev = new hytek.Mtevent(row);
+            final var mtevent = new HashMap<Integer, hytek.tm.Mtevent>();
+            for (final Row row : db.getTable(hytek.tm.Mtevent.NAME)) {
+                hytek.tm.Mtevent ev = new hytek.tm.Mtevent(row);
                 mtevent.put(ev.getMtevent(), ev);
-                //System.out.println(ev);
             }
 
 
             System.out.println("Teams: " + teams.size());
 
             // get athletes
-            final var athletes = new HashMap<Integer, hytek.Athlete>();
-            for (final Row row : db.getTable(hytek.Athlete.NAME)) {
-                hytek.Athlete a = new Athlete(row);
+            final var athletes = new HashMap<Integer, hytek.tm.Athlete>();
+            for (final Row row : db.getTable(hytek.tm.Athlete.NAME)) {
+                hytek.tm.Athlete a = new hytek.tm.Athlete(row);
                 athletes.put(a.getAthlete(), a);
             }
             System.out.println("Athletes: " + athletes.size());
 
             // track swins my athlete by event
             //         Athtlete         Event
-            final Map<Integer, HashMap<Short, Result>> swims = new HashMap<>();
+            final Map<Integer, HashMap<Short, hytek.tm.Result>> swims = new HashMap<>();
 
 
             // relays are also athletes
-            final var relays = new HashMap<Integer, hytek.Relay>();
-            for (var row : db.getTable(Relay.NAME)) {
-                hytek.Relay a = new Relay(row);
+            final var relays = new HashMap<Integer, hytek.tm.Relay>();
+            for (var row : db.getTable(hytek.tm.Relay.NAME)) {
+                hytek.tm.Relay a = new hytek.tm.Relay(row);
                 relays.put(a.getRelay(), a);
             }
 
             System.out.println("Getting Best Times.");
-            for (final Row row : db.getTable(Result.NAME)) {
-                final Result result = new hytek.Result(row);
+            for (final Row row : db.getTable(hytek.tm.Result.NAME)) {
+                var result = new hytek.tm.Result(row);
 
                 if (0 == result.getScore()) {
                     continue;
@@ -86,7 +83,7 @@ public final class Mock {
 
             // add only ome time per event
 
-            final Map<Integer, HashMap<Integer, ArrayList<hytek.Result>>> teamAthResults = new HashMap<>();
+            final Map<Integer, HashMap<Integer, ArrayList<hytek.tm.Result>>> teamAthResults = new HashMap<>();
 
 
             for (var a : swims.keySet()) {
@@ -115,7 +112,7 @@ public final class Mock {
                 for (var team2 : teams.keySet()) {
                     if (!Objects.equals(team1, team2)) {
 
-                        System.out.print(" " + teams.get(team1).getTcode() + " " + teams.get(team2).getTcode());
+                        System.out.print(teams.get(team1).getTcode() + "," + teams.get(team2).getTcode());
                         List<Integer> mockTeams = new ArrayList<Integer>();
                         mockTeams.add(team1);
                         mockTeams.add(team2);
@@ -131,15 +128,38 @@ public final class Mock {
 
 
     public static void runMockMeet(List<Integer> teams,
-                                   Map<Integer, HashMap<Integer, ArrayList<hytek.Result>>> teamAthResults,
-                                   HashMap<Integer, hytek.Athlete> athletes,
-                                   HashMap<Integer, hytek.Relay> relays,
-                                   HashMap<Integer, hytek.Mtevent> mtevent
+                                   Map<Integer, HashMap<Integer, ArrayList<hytek.tm.Result>>> teamAthResults,
+                                   HashMap<Integer, hytek.tm.Athlete> athletes,
+                                   HashMap<Integer, hytek.tm.Relay> relays,
+                                   HashMap<Integer, hytek.tm.Mtevent> mtevent
     ) {
         Map<Integer, Integer> teamScores = new TreeMap<Integer, Integer>();
 
 
-        Map<Short, TreeMap<Integer, ArrayList<hytek.Result>>> meet = new java.util.TreeMap<>();
+        final Map<Short, Integer[]> eventPoints = new HashMap<>();
+
+        Integer[] ind = {50,30,10};
+        Integer[] relay = {80,40,20};
+
+        // set all events to ind
+        for (Short e = 1; e <= 68; e++) {
+            eventPoints.put(e, ind);
+        }
+        // set relay events
+
+        Short[] relayE = {31,32,33,34,67,68};
+
+        for( Short r : relayE) {
+            eventPoints.put(r, relay);
+        }
+
+        //
+        //
+        //
+
+        Map<Short, TreeMap<Integer, ArrayList<hytek.tm.Result>>> meet = new java.util.TreeMap<>();
+        Map<Integer, Integer> scored = new HashMap<>();
+
         for (var tt : teams) {
             teamScores.put(tt, 0);
             var ath1 = teamAthResults.get(tt);
@@ -147,74 +167,54 @@ public final class Mock {
                 continue;
 
 
-            //
-            // can only enter 4 events
-            //
-
-            Map<Integer, Integer> scored= new HashMap<>();
-
-
-
             for (var a : ath1.keySet()) {
-                var scored = 0;
+                scored.put(a, 0);
                 for (var r : ath1.get(a)) {
-
-                    if (scored.get(a) == 4)  {
-                        System.out.println(a + " " + scored.get(a));
-                        continue;
-                    }
-                            
-
                     var ev = mtevent.get(r.getMtevent()).getMtev();
-
                     // add event
                     final var meetEvent = meet.computeIfAbsent(ev, k -> new TreeMap<>());
-
-
                     var time = r.getScore();
                     if ("Y".equals(r.getCourse())) {
                         time = (int) (time * 1.11);
                     }
-                    
                     final var meetEventScore = meetEvent.computeIfAbsent(time, k -> new ArrayList<>());
                     meetEventScore.add(r);
-                    scored.put(a, scored.get(a) + 1);
                 }
             }
         }
         for (final var event : meet.keySet()) {
             // determine points for places
-            // take top three times
+            //
 
+            var eventScore = eventPoints.get(event);
             final List<Integer> scores = new ArrayList<>(meet.get(event).keySet());
 
-            //System.out.print(event + " - ");
 
 
-            for (int s = 0; s < Math.min(3, scores.size()); s++) {
+
+            for (int s = 0; s < scores.size(); s++) {
                 final var values = meet.get(event).get(scores.get(s));
                 //System.out.print(scores.get(s) + "(" + values.size() + ") ");
 
-                var result = values.get(0);
+                if (s < eventScore.length) {
+                    final var points = eventScore[s];
+                    final var result = values.get(0);
 
-                // find team
-                Integer team = 0;
-                var ath = athletes.get(result.getAthlete());
-                final var relay = relays.get(result.getAthlete());
+                    // find team
+                    Integer team = 0;
+                    var ath = athletes.get(result.getAthlete());
+                    final var r = relays.get(result.getAthlete());
 
-                if (ath != null) {
-                    team = ath.getTeam1();
-                } else {
-                    team = relay.getTeam();
+                    if (ath != null) {
+                        team = ath.getTeam1();
+                        scored.put(r, scores.get(r) + 1);
+                    } else {
+                        team = r.getTeam();
+                    }
+
+
+                    teamScores.put(team, teamScores.get(team) + points);
                 }
-                var points = 0;
-                if (s == 0)
-                    points = 5;
-                else if (s == 1)
-                    points = 3;
-                else if (s == 3)
-                    points = 1;
-                teamScores.put(team, teamScores.get(team) + points);
             }
             //System.out.println();
         }
@@ -224,7 +224,7 @@ public final class Mock {
         var team2score = teamScores.get(teams.get(1));
 
 
-        System.out.println(" {" + team1score + ", " + team2score + "}" + (team1score - team2score));
+        System.out.println("," + team1score + "," + team2score + "," + (team1score - team2score)/10.0);
 
     }
 }
